49336613a4a75645a1d956a8a38dbf4722c725d1
Sandra Román <sandraroman912@gmail.com>
2023-05-15 22:38:36 +0200

    Update ClinicaDentista.sql

--- Server/ClinicaDentista.sql
+++ Server/ClinicaDentista.sql
@@ -1,5 +1,5 @@
 -- -----------------------------------------------------
--- DATABASE ClinicaDentista
+-- DATABASE 'ClinicaDentista'
 -- -----------------------------------------------------
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
@@ -11,56 +11,57 @@ CREATE DATABASE ClinicaDentista;
 USE ClinicaDentista;
 
 -- -----------------------------------------------------
--- Table `ClinicaDentista`.`Clinic`
+-- Table 'ClinicaDentista'.'Clinic'
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ClinicaDentista`.`Clinic` (
-  `cif` VARCHAR(15) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `address` VARCHAR(45) NOT NULL,
-  `phoneNumber` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  UNIQUE INDEX `cif_UNIQUE` (`cif` ASC) VISIBLE,
-  PRIMARY KEY (`cif`))
+CREATE TABLE IF NOT EXISTS 'ClinicaDentista'.'Clinic' (
+  'cif' VARCHAR(15) NOT NULL,
+  'name' VARCHAR(45) NOT NULL,
+  'address' VARCHAR(45) NOT NULL,
+  'phoneNumber' VARCHAR(45) NOT NULL,
+  'email' VARCHAR(45) NOT NULL,
+  UNIQUE INDEX 'cif_UNIQUE' ('cif' ASC) VISIBLE,
+  PRIMARY KEY ('cif'))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Patient`
+-- Table 'ClinicaDentista'.'Patient'
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ClinicaDentista`.`Patient` (
-  `dni` VARCHAR(9) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `phoneNumber` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `bornDate` DATE NOT NULL,
-  `clinic` VARCHAR(15) NOT NULL,
-  PRIMARY KEY (`dni`),
-  UNIQUE INDEX `dni_UNIQUE` (`dni` ASC) VISIBLE,
-  INDEX `clinic_idx` (`clinic` ASC) VISIBLE,
-  CONSTRAINT `clinic`
-    FOREIGN KEY (`clinic`)
-    REFERENCES `ClinicaDentista`.`Clinic` (`cif`)
+CREATE TABLE IF NOT EXISTS 'ClinicaDentista'.'Patient' (
+  'dni' VARCHAR(9) NOT NULL,
+  'name' VARCHAR(45) NOT NULL,
+  'surname' VARCHAR(45) NOT NULL,
+  'phoneNumber' VARCHAR(45) NOT NULL,
+  'email' VARCHAR(45) NOT NULL,
+  'bornDate' DATE NOT NULL,
+  'clinic' VARCHAR(15) NOT NULL,
+  PRIMARY KEY ('dni'),
+  UNIQUE INDEX 'dni_UNIQUE' ('dni' ASC) VISIBLE,
+  INDEX 'clinic_idx' ('clinic' ASC) VISIBLE,
+  CONSTRAINT 'clinic'
+    FOREIGN KEY ('clinic')
+    REFERENCES 'ClinicaDentista'.'Clinic' ('cif')
     ON DELETE RESTRICT
     ON UPDATE RESTRICT)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ClinicaDentista`.`Treatment`
+-- Table 'ClinicaDentista'.'Treatment'
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ClinicaDentista`.`Treatment` (
-  `code` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(100) NOT NULL,
-  `date` DATE NOT NULL,
-  `price` DECIMAL(10,2) NOT NULL,
-  `isPaid` TINYINT NOT NULL,
-  `patient` VARCHAR(9) NOT NULL,
-  PRIMARY KEY (`code`),
-  UNIQUE INDEX `code_UNIQUE` (`code` ASC) VISIBLE,
-  INDEX `patient_idx` (`patient` ASC) VISIBLE,
-  CONSTRAINT `patient`
-    FOREIGN KEY (`patient`)
-    REFERENCES `ClinicaDentista`.`Patient` (`dni`)
+CREATE TABLE IF NOT EXISTS 'ClinicaDentista'.'Treatment' (
+  'code' VARCHAR(45) NOT NULL,
+  'description' VARCHAR(100) NOT NULL,
+  'date' DATE NOT NULL,
+  'price' DECIMAL(10,2) NOT NULL,
+  'isPaid' BOOLEAN NOT NULL,
+  'patient' VARCHAR(9) NOT NULL,
+  PRIMARY KEY ('code'),
+  UNIQUE INDEX 'code_UNIQUE' ('code' ASC) VISIBLE,
+  INDEX 'patient_idx' ('patient' ASC) VISIBLE,
+  CONSTRAINT 'patient'
+    FOREIGN KEY ('patient')
+    REFERENCES 'ClinicaDentista'.'Patient' ('dni')
     ON DELETE RESTRICT
     ON UPDATE RESTRICT)
 ENGINE = InnoDB;
@@ -70,7 +71,7 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- USER `principal`@`%`
+-- USER 'principal'@'%'
 -- -----------------------------------------------------
 CREATE USER 'principal'@'%' IDENTIFIED BY '1234';
 
