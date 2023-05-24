aac3a62ba2b5501af3971c161ca59eb7dd96318f
AdriAir <aborio2004@gmail.com>
2023-05-24 08:41:00 +0200

    Lifecycle fix

--- Backend/src/main/java/com/politecnicomalaga/controller/ClinicServlet.java
+++ Backend/src/main/java/com/politecnicomalaga/controller/ClinicServlet.java
@@ -1,14 +1,12 @@
 package com.politecnicomalaga.controller;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/Clinica")
 public class ClinicServlet extends HttpServlet {
     //HTTP SERVLET
 
